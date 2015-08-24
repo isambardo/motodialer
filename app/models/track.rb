2f9e22e6ac4f121c8e2e1d53dedd446b724ea4d3
isambardo <kjanssen@MSPediKjanssen-2.local>
2015-08-24 10:09:30 -0500

    validates track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,5 @@
 class Track < ActiveRecord::Base
   has_many :set_ups , :class_name => "SetUp", :foreign_key => "track_id"
+  validates :name, :presence => true, :uniqueness => true
+  validates :address, :presence => true
 end
