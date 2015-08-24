2908d3d83b9412ff2d4c1bd48cfffc96b2f81f2c
isambardo <kjanssen@MSPediKjanssen-2.local>
2015-08-24 10:18:52 -0500

    validates bike model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,4 +1,8 @@
 class Bike < ActiveRecord::Base
 has_many :set_ups , :class_name => "SetUp", :foreign_key => "bike_id"
 belongs_to :user
+validates :model, :presence => true
+validates :manufacturer, :presence => true
+validates :year, :presence => true, numericality: { greater_than: 1900 }, numericality: { less_than: 2050 }
+
 end
